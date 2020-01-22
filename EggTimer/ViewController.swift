96f904611c0b004d147480e0daa23b8d5c381b3a
Joshua Ronaldo <joshuaronaldo088@gmail.com>
2020-01-22 13:35:49 +0700

    finished the chapter with adding timer function and alarm sound when the timer is stop

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    var musicPlayer: AVAudioPlayer?
+    
+    @IBOutlet weak var timerProgressView: UIProgressView!
+    
+    let eggTimes : [String:Int] = ["Soft" : 10, "Medium" : 420, "Hard" : 720]
+    
+    var secondsRemaining : Int = 0
+    var totalTimes : Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    func playSound(){
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else{
+                   return print("Wrong extension file")
+               }
+               musicPlayer = try! AVAudioPlayer(contentsOf: url)
+               musicPlayer?.play()
+    }
+    
+    @IBAction func hardnessSelected (_ sender: UIButton){
+        print("\(sender.currentTitle!)")
+        
+        let hardness = sender.currentTitle!
+        
+        totalTimes = eggTimes["\(hardness)"]!
+        
+        
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+           }
+        
+           @objc func update(){
+               if secondsRemaining < totalTimes {
+                    print("\(secondsRemaining) seconds")
+                print(totalTimes)
+                   secondsRemaining += 1
+                timerProgressView.progress = Float((Float(secondsRemaining) / Float(totalTimes)))
+                
+                print(timerProgressView.progress)
+                
+               } else {
+                playSound()
+            }
+            
+        
+    }
 }
